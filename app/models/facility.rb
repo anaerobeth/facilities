d4c2d8d603a8da33a0da911bab151fa83e05dc71
Beth Tenorio <betsky98@gmail.com>
2013-11-09 21:34:47 -0500

    Add validation for facility model

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,7 @@
 class Facility < ActiveRecord::Base
+  validates_presence_of(:category)
+  validates_numericality_of(:capacity)
+
+  validates_presence_of(:label)
+
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -5,7 +5,10 @@ describe Location do
   it { should have_valid(:category).when("Manhole", "Hut", "Building", "Rack") }
   it { should_not have_valid(:category).when("", "bananas", nil) }
   it { should validate_presence_of(:label) }
-  it { should validate_presence_of(:landlord) }
+
+  it { should have_valid(:landlord_id).when(4,12)  }
+  it { should_not have_valid(:landlord_id).when('', nil)  }
+
   it { should validate_numericality_of(:longitude) }
   it { should validate_numericality_of(:latitude) }
   it { should validate_presence_of(:address) }
